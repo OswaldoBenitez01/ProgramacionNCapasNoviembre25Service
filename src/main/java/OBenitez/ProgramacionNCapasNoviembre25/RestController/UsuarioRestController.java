de30007c33d71279e1ca37d6abaf82467fb7b266
Oswaldo Benitez Camacho <oswaldobenitez01@outlook.com>
2026-04-07 10:21:35 -0600

    Corrigiendo errores

--- src/main/java/OBenitez/ProgramacionNCapasNoviembre25/RestController/UsuarioRestController.java
+++ src/main/java/OBenitez/ProgramacionNCapasNoviembre25/RestController/UsuarioRestController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
